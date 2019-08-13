3b549719711d712023531299ddb12e69ca19e5e8
Jun Sok Huhh <anarinsk@gmail.com>
2019-08-13 10:48:48 +0900

    editing & typo

--- tested.R
+++ tested.R
@@ -1,7 +1,11 @@
 # https://dhicks.github.io/2018-10-10-did/
 
-xfun::pkg_attach2('haven', 'skimr')
+xfun::pkg_attach2('haven', 'skimr', 'tidyverse')
 
 dataf = haven::read_dta('data/eitc.dta')
 skim(dataf, work, year, children) %>% 
   skimr::kable(digits = 0)
+
+dataf = dataf %>%
+  mutate(post93 = year >= 1994, 
+         anykids = children >= 1)
